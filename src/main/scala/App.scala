b0a5a45d136ff2a668d78d9ece8f5f4d83fe7f66
Mathis Randl <mathis.randl@epfl.ch>
2021-12-29 21:49:31 +0100

    higher limit

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -3,11 +3,11 @@ object App {
   import scalanative.unsigned.given
 
   @main def main() = 
-    val limit = 100
+    val limit = 1000
     Zone { implicit allocator =>
       val ptr = alloc[BigInt](limit.toULong)
       for i <- (0 until limit) do {
-        ptr(i) = BigInt(2).pow(200+ i)
+        ptr(i) = BigInt(2).pow(i)
       }
       for i <- (0 until limit) do {
         println(s"2 to the ${i} : " + ptr(i))
